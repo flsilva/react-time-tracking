33223c314a997761984e4ad198e9428b87d61e6f
Flavio Silva <flsilva7@gmail.com>
2017-09-21 17:48:06 -0300

    refactor UserActions and UserReducers.

--- src/behavior/app/users/UserActions.js
+++ src/behavior/app/users/UserActions.js
@@ -1,6 +1,2 @@
-export const USER_LOGGED_IN = 'USER_LOGGED_IN';
-export const USER_LOGGED_OUT = 'USER_LOGGED_OUT';
+// eslint-disable-next-line import/prefer-default-export
 export const UPDATE_DATABASE = 'UPDATE_DATABASE';
-
-export const userLoggedIn = () => ({ type: USER_LOGGED_IN });
-export const userLoggedOut = () => ({ type: USER_LOGGED_OUT });
--- src/behavior/app/users/UserReducers.js
+++ src/behavior/app/users/UserReducers.js
@@ -1,34 +1,16 @@
 import merge from 'lodash/merge';
 import {
-  USER_LOGGED_IN,
-  USER_LOGGED_OUT,
   UPDATE_DATABASE,
 } from './UserActions';
 import { SIGN_OUT_SUCCESS } from '../auth/sign-out/SignOutActions';
 
+// eslint-disable-next-line import/prefer-default-export
 export const entities = (state = {}, action) => {
   switch (action.type) {
     case UPDATE_DATABASE:
       return merge(state, action.payload);
 
     case SIGN_OUT_SUCCESS:
-      return null;
-
-    default:
-      return state;
-  }
-};
-
-export default (state = {}, action) => {
-  console.log('UserReducers().users()');
-  console.log('state:', state);
-  console.log('action:', action);
-
-  switch (action.type) {
-    case USER_LOGGED_IN:
-      return action.payload;
-
-    case USER_LOGGED_OUT:
       return {};
 
     default:
