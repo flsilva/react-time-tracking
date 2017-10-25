flsilva/react-time-tracking

commit 429de6e75427fce388dc606afa08228f905e8bfa

refactoring: add new StopwatchUtils.js file, and move some logic there.

src/behavior/app/stopwatch/StopwatchActions.js
@@ -1,8 +1,11 @@
 import { put, select, takeLatest } from 'redux-saga/effects';
-import addSeconds from 'date-fns/add_seconds';
-import differenceInSeconds from 'date-fns/difference_in_seconds';
 import isDate from 'date-fns/is_date';
 import { getStopwatch } from './StopwatchState';
+import {
+  changeElapsedHours,
+  changeElapsedMinutes,
+  getElapsedTimeInSeconds,
+} from './StopwatchUtils';
 
 export const SET_ACTIVITY_DATE_REQUESTED = 'app/stopwatch/set/date/requested';
 export const SET_ACTIVITY_DESCRIPTION_REQUESTED = 'app/stopwatch/set/description/requested';
@@ -18,21 +21,6 @@ export const START_STOPWATCH_REQUESTED = 'app/stopwatch/start/requested';
 export const RESET_STOPWATCH_REQUESTED = 'app/stopwatch/reset/requested';
 export const UPDATE_STOPWATCH_FAILED = 'app/stopwatch/update/failed';
 export const UPDATE_DATABASE = 'app/stopwatch/update/database';
-
-const getTotalElapsedSeconds = (startedAt, activityTotalTime) => (
-  startedAt ?
-    differenceInSeconds(addSeconds(new Date(), activityTotalTime), startedAt) : activityTotalTime
-);
-
-const getTime = (startedAt, activityTotalTime) => {
-  const totalElapsedSeconds = getTotalElapsedSeconds(startedAt, activityTotalTime);
-  const hours = Math.floor(totalElapsedSeconds / 3600);
-  const minutes = Math.floor(totalElapsedSeconds / 60) - (hours * 60);
-  let seconds = totalElapsedSeconds - (minutes * 60) - (hours * 3600);
-  if (seconds < 10) seconds = `0${seconds}`;
-
-  return { hours, minutes, seconds };
-};
 
 const generatePatchHttpRequest = (id, payload = {}) => {
   if (!id) throw new Error('Argument <id> must not be null.');
@@ -91,14 +79,14 @@ export const pauseStopwatch = ({ id, startedAt, activityTotalTime = 0 }) => {
     type: PAUSE_STOPWATCH_REQUESTED,
     meta: {
       http: generatePatchHttpRequest(id, {
-        activityTotalTime: (activityTotalTime + differenceInSeconds(new Date(), startedAt)),
+        activityTotalTime: getElapsedTimeInSeconds(startedAt, new Date(), activityTotalTime),
         startedAt: null,
       }),
     },
   };
 };
 
-export const setStopwatchHours = ({ id, startedAt, activityTotalTime = 0, hours = 0 }) => {
+export const setStopwatchHours = ({ id, activityTotalTime = 0, hours = 0 }) => {
   if (!id) throw new Error('Argument <id> must not be null.');
   if (isNaN(activityTotalTime)) throw new Error('Argument <activityTotalTime> must be an integer.');
 
@@ -106,26 +94,17 @@ export const setStopwatchHours = ({ id, startedAt, activityTotalTime = 0, hours 
     throw new Error('Argument <hours> must be an integer.');
   }
 
-  const currentHours = getTime(startedAt, activityTotalTime).hours;
-
-  let newActivityTotalTime = activityTotalTime;
-  if (hours > currentHours) {
-    newActivityTotalTime += (hours - currentHours) * 3600;
-  } else {
-    newActivityTotalTime -= (currentHours - hours) * 3600;
-  }
-
   return {
     type: SET_STOPWATCH_HOURS_REQUESTED,
     meta: {
       http: generatePatchHttpRequest(id, {
-        activityTotalTime: newActivityTotalTime,
+        activityTotalTime: changeElapsedHours(activityTotalTime, hours),
       }),
     },
   };
 };
 
-export const setStopwatchMinutes = ({ id, startedAt, activityTotalTime = 0, minutes = 0 }) => {
+export const setStopwatchMinutes = ({ id, activityTotalTime = 0, minutes = 0 }) => {
   if (!id) throw new Error('Argument <id> must not be null.');
   if (isNaN(activityTotalTime)) throw new Error('Argument <activityTotalTime> must be an integer.');
 
@@ -133,20 +112,11 @@ export const setStopwatchMinutes = ({ id, startedAt, activityTotalTime = 0, minu
     throw new Error('Argument <minutes> must be an integer.');
   }
 
-  const currentMinutes = getTime(startedAt, activityTotalTime).minutes;
-
-  let newActivityTotalTime = activityTotalTime;
-  if (minutes > currentMinutes) {
-    newActivityTotalTime += (minutes - currentMinutes) * 60;
-  } else {
-    newActivityTotalTime -= (currentMinutes - minutes) * 60;
-  }
-
   return {
     type: SET_STOPWATCH_MINUTES_REQUESTED,
     meta: {
       http: generatePatchHttpRequest(id, {
-        activityTotalTime: newActivityTotalTime,
+        activityTotalTime: changeElapsedMinutes(activityTotalTime, minutes),
       }),
     },
   };

src/behavior/app/stopwatch/StopwatchTimeAndControlsContainer.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as StopwatchActions from './StopwatchActions';
 import { getStopwatch } from './StopwatchState';
+import { getElapsedTimeObject } from './StopwatchUtils';
 import StopwatchTimeAndControls from '../../../ui/app/stopwatch/StopwatchTimeAndControls';
 
 const StopwatchTimeAndControlsContainer = (props) => {
@@ -17,18 +18,19 @@ const StopwatchTimeAndControlsContainer = (props) => {
   };
 
   const setStopwatchHours = (hours) => {
-    const { id, activityTotalTime, startedAt } = props;
-    props.actions.setStopwatchHours({ id, activityTotalTime, hours, startedAt });
+    const { id, activityTotalTime } = props;
+    props.actions.setStopwatchHours({ id, activityTotalTime, hours });
   };
 
   const setStopwatchMinutes = (minutes) => {
-    const { id, activityTotalTime, startedAt } = props;
-    props.actions.setStopwatchMinutes({ id, activityTotalTime, minutes, startedAt });
+    const { id, activityTotalTime } = props;
+    props.actions.setStopwatchMinutes({ id, activityTotalTime, minutes });
   };
 
   return (
     <StopwatchTimeAndControls
       activityTotalTime={props.activityTotalTime}
+      getElapsedTimeObject={getElapsedTimeObject}
       isConnecting={props.isConnecting}
       isRunning={props.isRunning}
       onHourPick={setStopwatchHours}

src/behavior/app/stopwatch/StopwatchUtils.js
@@ -1,0 +1,48 @@
+import addSeconds from 'date-fns/add_seconds';
+import differenceInSeconds from 'date-fns/difference_in_seconds';
+import isDate from 'date-fns/is_date';
+
+export const getElapsedTimeInSeconds = (start, end, offset = 0) => {
+  if (isDate(start) && !isDate(end)) {
+    throw new Error('Argument <end> must not be null when providing argument <start>.');
+  }
+
+  return start ? differenceInSeconds(addSeconds(end, offset), start) : offset;
+};
+
+export const getElapsedHours = (start, end, offset = 0) => (
+  Math.floor(getElapsedTimeInSeconds(start, end, offset) / 3600)
+);
+
+export const getElapsedMinutes = (start, end, offset = 0) => {
+  const elapsedTimeInSeconds = Math.floor(getElapsedTimeInSeconds(start, end, offset));
+  const elapsedHours = getElapsedHours(start, end, offset);
+  return Math.floor(elapsedTimeInSeconds / 60) - (elapsedHours * 60);
+};
+
+export const getElapsedSeconds = (start, end, offset = 0) => {
+  const elapsedTimeInSeconds = getElapsedTimeInSeconds(start, end, offset);
+  const elapsedHours = getElapsedHours(start, end, offset);
+  const elapsedMinutes = getElapsedMinutes(start, end, offset);
+
+  return elapsedTimeInSeconds - (elapsedMinutes * 60) - (elapsedHours * 3600);
+};
+
+export const getElapsedTimeObject = (start, end, offset = 0) => {
+  const hours = getElapsedHours(start, end, offset);
+  const minutes = getElapsedMinutes(start, end, offset);
+  const seconds = getElapsedSeconds(start, end, offset);
+
+  return { hours, minutes, seconds };
+};
+
+export const changeElapsedHours = (timeInSeconds, hours) => {
+  const currentHours = Math.floor(timeInSeconds / 3600);
+  return timeInSeconds + ((hours - currentHours) * 3600);
+};
+
+export const changeElapsedMinutes = (timeInSeconds, minutes) => {
+  const currentHours = Math.floor(timeInSeconds / 3600);
+  const currentMinutes = Math.floor(timeInSeconds / 60) - (currentHours * 60);
+  return timeInSeconds + ((minutes - currentMinutes) * 60);
+};

src/ui/app/stopwatch/StopwatchTimeAndControls.js
@@ -43,6 +43,7 @@ const StopwatchTimeAndControls = (props) => {
       </div>
       <TimeElapsed
         activityTotalTime={activityTotalTime}
+        getElapsedTimeObject={props.getElapsedTimeObject}
         onHourPick={props.onHourPick}
         isRunning={isRunning}
         onMinutePick={props.onMinutePick}
@@ -60,6 +61,7 @@ StopwatchTimeAndControls.contextTypes = {
 
 StopwatchTimeAndControls.propTypes = {
   activityTotalTime: PropTypes.number,
+  getElapsedTimeObject: PropTypes.func.isRequired,
   isRunning: PropTypes.bool,
   startedAt: PropTypes.instanceOf(Date),
   onHourPick: PropTypes.func.isRequired,

src/ui/app/stopwatch/TimeElapsed.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
-import addSeconds from 'date-fns/add_seconds';
-import differenceInSeconds from 'date-fns/difference_in_seconds';
 
 const styles = {
   container: {
@@ -46,7 +44,7 @@ class TimeElapsed extends Component {
   state = {
     hours: 0,
     minutes: 0,
-    seconds: '00',
+    seconds: 0,
   }
 
   // componentWillMount() is needed because when we navigate between screens
@@ -68,19 +66,10 @@ class TimeElapsed extends Component {
   }
 
   getTime = (startedAt, activityTotalTime) => {
-    const totalElapsedSeconds = this.getTotalElapsedSeconds(startedAt, activityTotalTime);
-    const hours = Math.floor(totalElapsedSeconds / 3600);
-    const minutes = Math.floor(totalElapsedSeconds / 60) - (hours * 60);
-    let seconds = totalElapsedSeconds - (minutes * 60) - (hours * 3600);
-    if (seconds < 10) seconds = `0${seconds}`;
-
-    return { hours, minutes, seconds };
+    const time = this.props.getElapsedTimeObject(startedAt, new Date(), activityTotalTime);
+    if (time.seconds < 10) time.seconds = `0${time.seconds}`;
+    return time;
   }
-
-  getTotalElapsedSeconds = (startedAt, activityTotalTime) => (
-    startedAt ?
-      differenceInSeconds(addSeconds(new Date(), activityTotalTime), startedAt) : activityTotalTime
-  );
 
   handleUpdate = (props) => {
     if (props.isRunning) {
@@ -149,6 +138,7 @@ class TimeElapsed extends Component {
 
 TimeElapsed.propTypes = {
   onHourPick: PropTypes.func.isRequired,
+  getElapsedTimeObject: PropTypes.func.isRequired,
   isRunning: PropTypes.bool,
   onMinutePick: PropTypes.func.isRequired,
   startedAt: PropTypes.instanceOf(Date),

commit f0e871017d921ecd30c5950450fcbc909f247ace

[fix] stopwatch: fix bug with setMinutes and setHours not considering startAt. bug introduced in commit 429de6e.

src/behavior/app/stopwatch/StopwatchActions.js
@@ -84,7 +84,7 @@ export const pauseStopwatch = ({ id, startedAt, activityTotalTime = 0 }) => {
   };
 };
 
-export const setStopwatchHours = ({ id, activityTotalTime = 0, hours = 0 }) => {
+export const setStopwatchHours = ({ id, activityTotalTime = 0, hours = 0, startedAt }) => {
   if (!id) throw new Error('Argument <id> must not be null.');
   if (isNaN(activityTotalTime)) throw new Error('Argument <activityTotalTime> must be an integer.');
 
@@ -96,13 +96,13 @@ export const setStopwatchHours = ({ id, activityTotalTime = 0, hours = 0 }) => {
     type: SET_STOPWATCH_HOURS_REQUESTED,
     meta: {
       http: generatePatchHttpRequest(id, {
-        activityTotalTime: changeElapsedHours(activityTotalTime, hours),
+        activityTotalTime: changeElapsedHours(startedAt, hours, activityTotalTime),
       }),
     },
   };
 };
 
-export const setStopwatchMinutes = ({ id, activityTotalTime = 0, minutes = 0 }) => {
+export const setStopwatchMinutes = ({ id, activityTotalTime = 0, minutes = 0, startedAt }) => {
   if (!id) throw new Error('Argument <id> must not be null.');
   if (isNaN(activityTotalTime)) throw new Error('Argument <activityTotalTime> must be an integer.');
 
@@ -114,7 +114,7 @@ export const setStopwatchMinutes = ({ id, activityTotalTime = 0, minutes = 0 }) 
     type: SET_STOPWATCH_MINUTES_REQUESTED,
     meta: {
       http: generatePatchHttpRequest(id, {
-        activityTotalTime: changeElapsedMinutes(activityTotalTime, minutes),
+        activityTotalTime: changeElapsedMinutes(startedAt, minutes, activityTotalTime),
       }),
     },
   };

src/behavior/app/stopwatch/StopwatchTimeAndControlsContainer.js
@@ -18,13 +18,13 @@ const StopwatchTimeAndControlsContainer = (props) => {
   };
 
   const setStopwatchHours = (hours) => {
-    const { id, activityTotalTime } = props;
-    props.actions.setStopwatchHours({ id, activityTotalTime, hours });
+    const { id, activityTotalTime, startedAt } = props;
+    props.actions.setStopwatchHours({ id, activityTotalTime, hours, startedAt });
   };
 
   const setStopwatchMinutes = (minutes) => {
-    const { id, activityTotalTime } = props;
-    props.actions.setStopwatchMinutes({ id, activityTotalTime, minutes });
+    const { id, activityTotalTime, startedAt } = props;
+    props.actions.setStopwatchMinutes({ id, activityTotalTime, minutes, startedAt });
   };
 
   return (

src/behavior/app/stopwatch/StopwatchUtils.js
@@ -36,13 +36,14 @@ export const getElapsedTimeObject = (start, end, offset = 0) => {
   return { hours, minutes, seconds };
 };
 
-export const changeElapsedHours = (timeInSeconds, hours) => {
-  const currentHours = Math.floor(timeInSeconds / 3600);
-  return timeInSeconds + ((hours - currentHours) * 3600);
+export const changeElapsedHours = (start, hours, offset = 0) => {
+  const now = new Date();
+  const currentHours = getElapsedHours(start, now, offset);
+  return offset + ((hours - currentHours) * 3600);
 };
 
-export const changeElapsedMinutes = (timeInSeconds, minutes) => {
-  const currentHours = Math.floor(timeInSeconds / 3600);
-  const currentMinutes = Math.floor(timeInSeconds / 60) - (currentHours * 60);
-  return timeInSeconds + ((minutes - currentMinutes) * 60);
+export const changeElapsedMinutes = (start, minutes, offset = 0) => {
+  const now = new Date();
+  const currentMinutes = getElapsedMinutes(start, now, offset);
+  return offset + ((minutes - currentMinutes) * 60);
 };