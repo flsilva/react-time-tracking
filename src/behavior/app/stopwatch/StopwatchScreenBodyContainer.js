136b0d855711755e58eb30e7ad89dd8e5f69ead4
Flavio Silva <flsilva7@gmail.com>
2017-10-26 18:39:29 -0200

    [refactoring] move WithIcon back to react function component, instead of wrapper function.

--- src/behavior/app/stopwatch/StopwatchScreenBodyContainer.js
+++ src/behavior/app/stopwatch/StopwatchScreenBodyContainer.js
@@ -3,26 +3,23 @@ import CalendarIcon from 'material-ui/svg-icons/action/event';
 import WorkIcon from 'material-ui/svg-icons/action/work';
 import SheetIcon from 'material-ui/svg-icons/action/description';
 import ScreenBody from '../../../ui/app/common/ScreenBody';
-import withIcon from '../../../ui/app/common/withIcon';
+import WithIcon from '../../../ui/app/common/WithIcon';
 import DatePickerContainer from './DatePickerContainer';
 import StopwatchProjectDropDownContainer from './StopwatchProjectDropDownContainer';
 import DescriptionButtonContainer from './DescriptionButtonContainer';
 import StopwatchFormButtonsContainer from './StopwatchFormButtonsContainer';
 
-const DatePickerIcon = () => <CalendarIcon color={'#3f2da5'} />;
-const DatePickerWithIcon = withIcon(DatePickerContainer, DatePickerIcon);
-
-const ProjectIcon = () => <WorkIcon color={'#3f2da5'} />;
-const ProjectDropDownWithIcon = withIcon(StopwatchProjectDropDownContainer, ProjectIcon);
-
-const DescriptionIcon = () => <SheetIcon color={'#3f2da5'} />;
-const DescriptionButtonWithIcon = withIcon(DescriptionButtonContainer, DescriptionIcon);
-
 const StopwatchScreenBodyContainer = () => (
   <ScreenBody>
-    <DatePickerWithIcon />
-    <ProjectDropDownWithIcon />
-    <DescriptionButtonWithIcon />
+    <WithIcon icon={<CalendarIcon color={'#3f2da5'} />}>
+      <DatePickerContainer />
+    </WithIcon>
+    <WithIcon icon={<WorkIcon color={'#3f2da5'} />}>
+      <StopwatchProjectDropDownContainer />
+    </WithIcon>
+    <WithIcon icon={<SheetIcon color={'#3f2da5'} />}>
+      <DescriptionButtonContainer />
+    </WithIcon>
     <StopwatchFormButtonsContainer />
   </ScreenBody>
 );
--- src/ui/app/common/withIcon.js
+++ src/ui/app/common/withIcon.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import stylePropType from 'react-style-proptype';
 
-const withIcon = (Component, Icon) => () => (
-  <div style={{ alignItems: 'center', display: 'flex', height: 48 }}>
-    <div style={{ alignItems: 'center', display: 'flex', marginRight: 20 }}>
-      <Icon />
+const WithIcon = ({ children, icon, iconStyle, style }) => (
+  <div style={{ alignItems: 'center', display: 'flex', height: 48, ...style }}>
+    <div style={{ alignItems: 'center', display: 'flex', marginRight: 20, ...iconStyle }}>
+      {icon}
     </div>
-    <Component />
+    {children}
   </div>
 );
 
-export default withIcon;
+WithIcon.propTypes = {
+  children: PropTypes.node.isRequired,
+  icon: PropTypes.element.isRequired,
+  iconStyle: stylePropType,
+  style: stylePropType,
+};
+
+WithIcon.defaultProps = {
+  iconStyle: undefined,
+  style: undefined,
+};
+
+export default WithIcon;
