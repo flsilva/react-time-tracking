aa5bb499868cd2da3b7b0c3de4a6e964818c7c13
Flavio Silva <flsilva7@gmail.com>
2017-11-10 21:58:24 -0200

    [refactoring] delete utils/withCrudForm.js, move logic back to projects/withFormik.js. not enough of an abstraction to justify it, better keep it simpler in this case.

--- src/behavior/app/projects/withFormik.js
+++ src/behavior/app/projects/withFormik.js
@@ -1,20 +1,60 @@
-import withCrudForm from '../utils/withCrudForm';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { createEntity, deleteEntity, updateEntity } from './ProjectActions';
 
-export default (getNavigator) => {
-  const successCb = () => {
-    getNavigator().push('/app/projects');
-  };
+export default getNavigator => (
+  (WrappedComponent) => {
+    function withEntityForm(props) {
+      const successCb = () => {
+        getNavigator().push('/app/projects');
+      };
 
-  const toFormValues = (entity = {}) => ({
-    name: entity.name || '',
-  });
+      const onSubmit = (values) => {
+        const { entity } = props;
 
-  return withCrudForm({
-    createEntity,
-    deleteEntity,
-    successCb,
-    toFormValues,
-    updateEntity,
-  });
-};
+        if (entity) {
+          props.updateEntity(entity.id, values, successCb);
+        } else {
+          props.createEntity(values, successCb);
+        }
+      };
+
+      const toFormValues = (entity = {}) => ({
+        name: entity.name || '',
+      });
+
+      return (
+        <WrappedComponent
+          {...props}
+          deleteEntity={() => props.deleteEntity(props.entity.id, successCb)}
+          initialValues={toFormValues(props.entity)}
+          onSubmit={onSubmit}
+          toFormValues={toFormValues}
+        />
+      );
+    }
+
+    withEntityForm.propTypes = {
+      createEntity: PropTypes.func.isRequired,
+      deleteEntity: PropTypes.func,
+      entity: PropTypes.shape({ id: PropTypes.string.isRequired }),
+      updateEntity: PropTypes.func.isRequired,
+    };
+
+    withEntityForm.defaultProps = {
+      deleteEntity: undefined,
+      entity: undefined,
+    };
+
+    const mapDispatchToProps = dispatch => ({
+      ...bindActionCreators({ createEntity, deleteEntity, updateEntity }, dispatch),
+    });
+
+    return connect(
+      undefined,
+      mapDispatchToProps,
+    )(withEntityForm);
+  }
+);
