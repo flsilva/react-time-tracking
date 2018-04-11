37923d478a3958bc20cc5e3e32a9656a37dafd14
Flavio Silva <flsilva7@gmail.com>
2018-04-11 11:40:31 -0300

    [cleanup] delete commented out code.

--- src/app/application/AppSideEffects.js
+++ src/app/application/AppSideEffects.js
@@ -1,5 +1,4 @@
 import { all } from 'redux-saga/effects';
-// import { clearCache } from './shared/net/http/caching/Services';
 import bindAuth from './auth/AuthSideEffects';
 import bindEmailSignIn from './auth/email/EmailSignInSideEffects';
 import bindEmailSignUp from './auth/email/EmailSignUpSideEffects';
@@ -8,19 +7,9 @@ import bindRestoreSession from './auth/restore-session/RestoreSessionSideEffects
 import bindSignOut from './auth/sign-out/SignOutSideEffects';
 import bindStopwatch from './stopwatches/StopwatchSideEffects';
 import bindTimeLog from './time-logs/TimeLogSideEffects';
-// import { CLEAR_DATABASE as CLEAR_PROJECTS_DATABASE } from './projects/types';
-/*
-function* clearEntitiesSaga(entityType) {
-  yield put(clearCache(entityType));
-}
 
-function* bindApp() {
-  yield takeLatest(CLEAR_PROJECTS_DATABASE, clearEntitiesSaga, 'projects');
-}
-*/
 export default function* () {
   yield all([
-    // bindApp(),
     bindAuth(),
     bindEmailSignIn(),
     bindEmailSignUp(),
--- src/app/application/types.js
+++ src/app/application/types.js
@@ -6,6 +6,6 @@ import type { NetState } from './shared/net/Types';
 import type { ProjectState } from './projects/types';
 
 export type AppState = {
-  net: NetState,
-  projects: ProjectState
+  +net: NetState,
+  +projects: ProjectState
 };
